e76d5b0967864680e28fb1f1d15c23e201a08dc1
yueye <768242801@qq.com>
2022-01-28 16:38:05 +0800

    暂时解决网关启动不了问题-satoken注解鉴权失效

--- ruoyi-common/ruoyi-common-security/src/main/java/com/ruoyi/common/security/config/SaTokenConfiguration.java
+++ ruoyi-common/ruoyi-common-security/src/main/java/com/ruoyi/common/security/config/SaTokenConfiguration.java
@@ -1,19 +1,9 @@
 package com.ruoyi.common.security.config;
 
-import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
-import cn.dev33.satoken.interceptor.SaRouteInterceptor;
 import cn.dev33.satoken.jwt.StpLogicJwtForStyle;
-import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpLogic;
-import cn.hutool.core.util.ObjectUtil;
-import com.ruoyi.common.security.utils.LoginHelper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Sa-Token 整合 jwt (Style模式)
@@ -21,23 +11,23 @@ import javax.servlet.http.HttpServletResponse;
  * @author Lion Li
  */
 @Configuration
-public class SaTokenConfiguration implements WebMvcConfigurer {
+public class SaTokenConfiguration {
 
     /**
      * 注册sa-token的拦截器
      */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 注册路由拦截器，自定义验证规则
-        registry.addInterceptor(new SaRouteInterceptor() {
-            @SuppressWarnings("all")
-            @Override
-            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-                LoginHelper.clearCache();
-            }
-        }).addPathPatterns("/**");
-        registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 注册路由拦截器，自定义验证规则
+//        registry.addInterceptor(new SaRouteInterceptor() {
+//            @SuppressWarnings("all")
+//            @Override
+//            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+//                LoginHelper.clearCache();
+//            }
+//        }).addPathPatterns("/**");
+//        registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**");
+//    }
 
     @Bean
     public StpLogic getStpLogicJwt() {
