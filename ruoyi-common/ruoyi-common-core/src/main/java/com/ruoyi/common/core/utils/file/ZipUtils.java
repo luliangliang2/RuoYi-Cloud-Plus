2bff1e77b718feeb62ab16eda68fa7a95761a4db
bleachtred <bleachtred@163.com>
2022-09-17 18:40:44 +0800

    update 优化ZipUtils，对hutool.ZipUtil进行扩展，删除不必要的流读取

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/utils/file/ZipUtils.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/utils/file/ZipUtils.java
@@ -1,5 +1,6 @@
 package com.ruoyi.common.core.utils.file;
 
+import cn.hutool.core.util.ZipUtil;
 import cn.hutool.http.HttpUtil;
 import com.ruoyi.common.core.utils.StringUtils;
 import lombok.AccessLevel;
@@ -10,11 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -25,50 +22,13 @@ import java.util.zip.ZipOutputStream;
  * @author Bleachtred
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ZipUtils {
+public class ZipUtils extends ZipUtil {
 
     private final static Integer TEMP_SIZE = 2048;
 
     /**
-     * 压缩方法
-     * @param zipFile 压缩到本地的zip文件
-     * @param files 需要压缩的文件列表
-     * @author Bleachtred
-     */
-    public static void zipFilePip(File zipFile, File... files){
-        zipFilePip(zipFile, Arrays.asList(files));
-    }
-
-    /**
-     * 压缩方法
-     * @param zipFile 压缩到本地的zip文件名称带后缀
-     * @param files 需要压缩的文件列表
-     * @author Bleachtred
-     */
-    public static void zipFilePip(String zipFile, String ... files){
-        List<File> files1 = new ArrayList<>();
-        for(String fileStr : files){
-            files1.add(new File(fileStr));
-        }
-        zipFilePip(new File(zipFile),files1);
-    }
-
-    /**
-     * 压缩方法
-     * @param zipFile 压缩到本地的zip文件名称带后缀
-     * @param files 需要压缩的文件列表
-     * @author Bleachtred
-     */
-    public static void zipFilePip(String zipFile, List<String> files){
-        List<File> files1 = new ArrayList<>();
-        for(String fileStr : files){
-            files1.add(new File(fileStr));
-        }
-        zipFilePip(new File(zipFile),files1);
-    }
-
-    /**
      * 压缩方法 返回前端下载（支持 本地文件/目录 + oss路径 混合）
+     * @param downloadFilename 压缩包文件名 test.zip
      * @param files 文件列表
      * @author Bleachtred
      */
@@ -88,7 +48,7 @@ public class ZipUtils {
      * @param files 文件列表
      * @author Bleachtred
      */
-    public static void zipFilePip(List<String> files, HttpServletResponse response) {
+    private static void zipFilePip(List<String> files, HttpServletResponse response) {
 
         try(WritableByteChannel out = Channels.newChannel(response.getOutputStream())) {
             Pipe pipe = Pipe.open();
@@ -103,45 +63,6 @@ public class ZipUtils {
                     out.write(buffer);
                     buffer.clear();
                 }
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 压缩方法
-     * @param zipFile 压缩到本地的zip文件
-     * @param files 需要压缩的文件列表
-     * @author Bleachtred
-     */
-    public static void zipFilePip(File zipFile, List<File> files) {
-
-        try(WritableByteChannel out = Channels.newChannel(Files.newOutputStream(zipFile.toPath()))) {
-            Pipe pipe = Pipe.open();
-            //异步任务
-            CompletableFuture.runAsync(() -> runLocalTask(pipe,files));
-
-            //获取读通道
-            try (ReadableByteChannel readableByteChannel = pipe.source()) {
-                ByteBuffer buffer = ByteBuffer.allocate(TEMP_SIZE);
-                while (readableByteChannel.read(buffer) >= 0) {
-                    buffer.flip();
-                    out.write(buffer);
-                    buffer.clear();
-                }
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private static void runLocalTask(Pipe pipe, List<File> files) {
-
-        try(ZipOutputStream zos = new ZipOutputStream(Channels.newOutputStream(pipe.sink()));
-            WritableByteChannel out = Channels.newChannel(zos)) {
-            for (File file : files) {
-                taskFunction(zos, file, out, "");
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -199,15 +120,9 @@ public class ZipUtils {
             zos.putNextEntry(new ZipEntry(base));
 
             byte[] bytes = HttpUtil.downloadBytes(file);
-            InputStream fis = new ByteArrayInputStream(Objects.requireNonNull(bytes));
 
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = fis.read(buffer)) != -1) {
-                out.write(ByteBuffer.wrap(buffer, 0, len));
-            }
+            out.write(ByteBuffer.wrap(bytes));
 
-            fis.close();
         }else {
             taskFunction(zos, new File(file), out, "");
         }
