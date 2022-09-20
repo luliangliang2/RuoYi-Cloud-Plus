6a2cd40d0b3908105fbd73b2e40e499196d42cae
沫离 <974751082@qq.com>
2022-09-20 14:38:57 +0000

    add:新增seata undo_log建表语句 新增AT模式中，需要在参与全局事务的数据库中，添加一个undo_log表，此为建表语句
    Signed-off-by: 沫离 <974751082@qq.com>

---
+++ sql/seata_client_undo_log.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `undo_log` (
+  `branch_id` bigint(20) NOT NULL COMMENT 'branch transaction id',
+  `xid` varchar(100) NOT NULL COMMENT 'global transaction id',
+  `context` varchar(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+  `rollback_info` longblob NOT NULL COMMENT 'rollback info',
+  `log_status` int(11) NOT NULL COMMENT '0:normal status,1:defense status',
+  `log_created` datetime(6) NOT NULL COMMENT 'create datetime',
+  `log_modified` datetime(6) NOT NULL COMMENT 'modify datetime',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='AT transaction mode undo table'
