c322212715ea92c5350f991730a30040a025dd59
AprilWind <2100166581@qq.com>
2024-01-16 17:33:45 +0800

    add 新增 http客户端工具

---
+++ ruoyi-common/ruoyi-common-http/src/main/java/org/dromara/common/http/config/ForestConfiguration.java
@@ -0,0 +1,16 @@
+package org.dromara.common.http.config;
+
+import org.dromara.common.core.factory.YmlPropertySourceFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * HTTP 客户端框架配置
+ *
+ * @author Feng
+ */
+@AutoConfiguration
+@PropertySource(value = "classpath:common-forest.yml", factory = YmlPropertySourceFactory.class)
+public class ForestConfiguration {
+
+}
