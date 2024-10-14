e8bf451bd0babb669d98aa71d13b0c7a765a158c
bwy <87036052@qq.com>
2024-10-14 11:39:54 +0800

    style: 设置dubbo缓存目录为jar包所在目录

--- ruoyi-auth/src/main/java/org/dromara/auth/RuoYiAuthApplication.java
+++ ruoyi-auth/src/main/java/org/dromara/auth/RuoYiAuthApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.boot.system.ApplicationHome;
+
+import java.io.File;
 
 /**
  * 认证授权中心
@@ -15,6 +18,14 @@ import org.springframework.boot.context.metrics.buffering.BufferingApplicationSt
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuoYiAuthApplication {
     public static void main(String[] args) {
+        // 设置dubbo缓存目录为jar包所在目录
+        ApplicationHome home = new ApplicationHome(RuoYiAuthApplication.class);
+        String dirPath = home.getSource().getParentFile().toString();
+        String filePath = dirPath + File.separator + ".dubbo";
+        System.out.println("dubbo 缓存目录："+filePath);
+        System.setProperty("dubbo.meta.cache.filePath", filePath);
+        System.setProperty("dubbo.mapping.cache.filePath",filePath);
+
         SpringApplication application = new SpringApplication(RuoYiAuthApplication.class);
         application.setApplicationStartup(new BufferingApplicationStartup(2048));
         application.run(args);
